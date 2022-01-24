1b83e730642a8a7ad51d71db8f0fae9ca36eaee6
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-01-24 23:17:01 +0100

    feat: add css string generator draft

---
+++ src/lib/css.mjs
@@ -0,0 +1,36 @@
+import chalk from 'chalk'
+import handlebars from 'handlebars'
+
+const template = handlebars.compile(`
+@font-face {
+  font-family: "{{fontFamily}}";
+  src: url(data:font/truetype;charset=utf-8;base64,{{base64}});
+}
+{{#each classPrefixes}}
+[class^="{{this}}/"],
+{{#if @last}}[class*=" {{this}}/"]{{else}}[class*=" {{this}}/"],{{/if}}{{/each}} {
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  font-family: "{{fontFamily}}" !important;
+  font-size: {{fontSize}};
+  font-style: normal;
+}
+{{#each selectors}}
+.{{@key}}::before { content: "{{this}}"; }
+{{/each}}
+`)
+
+const css = opts => {
+  return template({
+    base64: 'aabbccddee',
+    classPrefixes: ['gm'],
+    fontFamily: 'omnicon',
+    selectors: [
+      { 'gm\/icon-name': '\ea01' },
+    ],
+  })
+}
+
+export {
+  css,
+}
