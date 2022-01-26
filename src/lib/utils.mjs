fba9cc415969bc4a25a6e011c3f2571d39cf99d6
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-01-26 09:44:40 +0100

    fix: add project name shared

--- src/lib/utils.mjs
+++ src/lib/utils.mjs
@@ -1,8 +1,8 @@
 import path from 'path'
+const PROJECT_NAME = 'iconsauce'
 const PROJECT_PATH = path.join('../')
 const TEMP_PATH = path.join(PROJECT_PATH, '.temp')
-const TEMP_CSS_PATH = path.join(TEMP_PATH, 'omnicon.css')
-const PROJECT_NAME = 'iconsauce'
+const TEMP_CSS_PATH = path.join(TEMP_PATH, `${PROJECT_NAME}.css`)
 
 export {
   PROJECT_NAME,
