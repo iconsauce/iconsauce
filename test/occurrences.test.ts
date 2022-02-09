b2021c0fc72c36032814af2e0414c58da70217e0
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-02-09 17:21:09 +0100

    test: fix async test

--- test/occurrences.test.ts
+++ test/occurrences.test.ts
@@ -1,23 +1,27 @@
+import { PathLike } from 'fs'
 import { occurrences } from '../src/lib/occurrences'
 import { defaultConfig } from '../src/lib/utils'
+import chalk from 'chalk'
 import files from './fixures/files'
+
+defaultConfig.verbose = true
 
 describe('Occurrences', () => {
   test('Check the selectors are properly loaded from source files', async () => {
-    return occurrences(defaultConfig, files)
-      .then(data => {
-        expect(data.map.get('gm/filled/close')).toMatch(/component-gm.tsx/)
-        expect(data.map.get('mdi/access-point')).toMatch(/component-mdi.tsx/)
-        expect(data.map.get('mgg/isbn')).toMatch(/component-mgg.tsx/)
-        expect(data.map.get('gm/filled/accessible-forward')).toMatch(/component.tsx/)
-        expect(data.map.get('mgg/todo-in-prendere-in-carico')).toMatch(/variants.ts/)
-        expect(new Set(data.occurrences)).toContain('gm/filled/non-existing-selector')
-      }).catch(error => {
-        console.error(error)
-      })
+    const data: { occurrences: string[], map: Map<string, PathLike> } = await occurrences(defaultConfig, files)
+    expect(data.map.get('gm/filled/close')).toMatch(/component-gm.tsx/)
+    expect(data.map.get('mdi/access-point')).toMatch(/component-mdi.tsx/)
+    expect(data.map.get('mgg/isbn')).toMatch(/component-mgg.tsx/)
+    expect(data.map.get('gm/filled/accessible-forward')).toMatch(/component.tsx/)
+    expect(data.map.get('mgg/todo-in-prendere-in-carico')).toMatch(/variants.ts/)
+    expect(new Set(data.occurrences)).toContain('gm/filled/non-existing-selector')
   })
 
   // test('Check the errors are thrown as expected', async () => {
-  //   return expect(async () => await occurrences(defaultConfig, ['non/exisising/file.tsx'])).toThrow(Error)
+  //   try {
+  //     await occurrences(defaultConfig, ['non/exisising/file.tsx'])
+  //   } catch (error) {
+  //     expect(error).toMatch(/Error/)
+  //   }
   // })
 })
