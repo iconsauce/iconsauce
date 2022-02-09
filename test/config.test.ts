a9c2be2532d222b96e3e2af9a972b26fa51a58e1
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-02-09 18:16:37 +0100

    test: import Config

--- test/config.test.ts
+++ test/config.test.ts
@@ -1,10 +1,11 @@
 import { LoadConfig } from '../src/lib/loadConfig'
 import { defaultConfig } from '../src/lib/utils'
+import { Config } from '../src/interface/config'
 import path from 'path'
 
 describe('Config', () => {
-  test('Check config loader creates a default config', () => {
-    const config = new LoadConfig(path.resolve(__dirname, './fixtures/iconsauce.config.js'))
+  test('Check LoadConfig loads a config', () => {
+    const config: Config = new LoadConfig(path.resolve(__dirname, './fixtures/iconsauce.config.js'))
     expect(config).toEqual(defaultConfig)
   })
 })
