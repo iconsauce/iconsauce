872da827e6e0f13a3eccb63291f51b7cf1070788
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-02-14 11:29:24 +0100

    lint: remove new lines

--- test/config.test.ts
+++ test/config.test.ts
@@ -6,13 +6,11 @@ import { configTest } from './fixtures/config'
 describe('Config', () => {
   test('Check LoadConfig loads a config', () => {
     const config: Config = new LoadConfig(path.resolve(__dirname, './fixtures/iconsauce.config.js'))
-
     expect(config.content).toEqual(configTest.content)
     expect(config.fontFamily).toEqual(configTest.fontFamily)
     expect(config.fontSize).toEqual(configTest.fontSize)
     expect(config.skipWarning).toEqual(configTest.skipWarning)
     expect(config.verbose).toEqual(configTest.verbose)
-
     expect(config.plugin.length).toEqual(configTest.plugin.length)
   })
 })
