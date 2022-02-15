137b24d923ae0c7c5e26c6bb68981a52c6720cf5
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-02-15 13:00:01 +0100

    fix: tests

--- test/fixtures/css-font.ts
+++ test/fixtures/css-font.ts
@@ -12,24 +12,24 @@ const cssString = String.raw`@font-face {
   text-align: center;
 }
 
-.gm\/filled\/10k::before { content: "\2f"}
-.gm\/filled\/1k::before { content: "\2f"}
-.gm\/filled\/1k-plus::before { content: "\2f"}
-.gm\/filled\/accessible-forward::before { content: "\2f"}
-.gm\/filled\/add-alert::before { content: "\2f"}
-.gm\/filled\/add-chart::before { content: "\2f"}
-.gm\/filled\/auto-delete::before { content: "\2f"}
-.gm\/filled\/close::before { content: "\2f"}
-.gm\/filled\/error::before { content: "\2f"}
-.gm\/filled\/error-outline::before { content: "\2f"}
-.gm\/filled\/info::before { content: "\2f"}
-.gm\/filled\/mic::before { content: "\2f"}
-.gm\/filled\/notification-important::before { content: "\2f"}
-.gm\/filled\/remove-shopping-cart::before { content: "\2f"}
-.gm\/filled\/transit-enterexit::before { content: "\2f"}
-.gm\/filled\/vpn-key::before { content: "\2f"}
-.gm\/filled\/warning::before { content: "\2f"}
-.gm\/filled\/warning-amber::before { content: "\2f"}
-.gm\/filled\/zoom-out-map::before { content: "\2f"}
+.gm\/filled\/10k::before { content: "\2f" }
+.gm\/filled\/1k::before { content: "\2f" }
+.gm\/filled\/1k-plus::before { content: "\2f" }
+.gm\/filled\/accessible-forward::before { content: "\2f" }
+.gm\/filled\/add-alert::before { content: "\2f" }
+.gm\/filled\/add-chart::before { content: "\2f" }
+.gm\/filled\/auto-delete::before { content: "\2f" }
+.gm\/filled\/close::before { content: "\2f" }
+.gm\/filled\/error::before { content: "\2f" }
+.gm\/filled\/error-outline::before { content: "\2f" }
+.gm\/filled\/info::before { content: "\2f" }
+.gm\/filled\/mic::before { content: "\2f" }
+.gm\/filled\/notification-important::before { content: "\2f" }
+.gm\/filled\/remove-shopping-cart::before { content: "\2f" }
+.gm\/filled\/transit-enterexit::before { content: "\2f" }
+.gm\/filled\/vpn-key::before { content: "\2f" }
+.gm\/filled\/warning::before { content: "\2f" }
+.gm\/filled\/warning-amber::before { content: "\2f" }
+.gm\/filled\/zoom-out-map::before { content: "\2f" }
 `
 export default cssString
