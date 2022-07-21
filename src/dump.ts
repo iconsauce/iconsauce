3b249cbe0be3faba54f23d83d31768b2499bdcc4
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-07-21 11:28:21 +0200

    fix: add missing dump log

--- src/dump.ts
+++ src/dump.ts
@@ -5,18 +5,22 @@ import { PathLike } from 'fs'
 import path from 'path'
 import copy from 'recursive-copy'
 
-const buildDump = async (configuration: Config, iconDictionary: Map<string, PathLike>, outputPath: PathLike): Promise<void> => {
+const buildDump = async (configuration: Config, iconsDictionary: Map<string, PathLike>, outputPath: PathLike): Promise<void> => {
   const config: Config = configuration ?? new IconsauceConfig()
 
-  for (const key of iconDictionary.keys()) {
-    await copy(path.resolve(iconDictionary.get(key) as string), path.resolve(path.join(outputPath.toString(), `${key}.svg`)), { overwrite: true })
+  if (config.verbose) {
+    console.info(`Dumping ${chalk.yellow(iconsDictionary.size)} SVG files, please wait...`)
+  }
+
+  for (const key of iconsDictionary.keys()) {
+    await copy(path.resolve(iconsDictionary.get(key) as string), path.resolve(path.join(outputPath.toString(), `${key}.svg`)), { overwrite: true })
       .catch(error => {
         throw Error(chalk.red(error))
       })
   }
 
   if (config.verbose) {
-    console.info(`Build of dump SVGs finished ${chalk.green('successfully')}`)
+    console.info(`SVGs dump finished ${chalk.green('successfully')}`)
   }
 
 }
